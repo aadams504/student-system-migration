f9211c1fe8645ee8c578db4a46848d8d94084742
Arianna <95667502+aadams504@users.noreply.github.com>
2022-04-18 04:31:39 -0700

    Initial Commit

---
+++ OA-C867/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  OA-C867
+//
+//  Created by Arianna Adams on 4/18/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
